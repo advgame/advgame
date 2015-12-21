f9eb2b0a0b986f420fcb3625e883dcc2ae1d0f4a
advgame <tsmithwd+advgit@gmail.com>
2015-12-21 10:40:34 -0500

    Add context.loadGame() to parse.js
    Add partially complete context.loadGame function to /js/parse.js

--- js/parse.js
+++ js/parse.js
@@ -1,3 +1,26 @@
 (function(context) {
   
+  context.loadGame = function(game, callback) {
+    game += ''; // Convert game to a string
+    // TODO: check if element is HTMLElement
+    // see: http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
+    
+    var request = new context.XMLHttpRequest();
+    request.open('GET', 'games/' + context.encodeURIComponent(game) + '.json');
+    // TODO
+    
+    return {
+      input: function(in) {
+        var out;
+        // TODO
+        return out;
+      },
+      reset: function() {
+        // TODO
+      },
+      getSave: function() {
+        // TODO
+      }
+    };
+  };
 })(window);
