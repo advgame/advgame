3d6d6a354e0fd783e24a134f5455badbd2a013e8
advgame <tsmithwd+advgit@gmail.com>
2015-12-21 18:57:33 -0500

    Add input/output support in main.js
    When enter key is pressed, parse input and add output in js/main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  var game = window.loadGame('plot');
   $('#input').keyup(function(e) {
     if (e.which === 13) {
-      // TODO
+      var input = this.value;
+      this.value = '';
+      $('#output').val($('#output').val() + game.input(input));
     }
   })
 });
