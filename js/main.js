15a63d13e7d7bbb17ba388e87745426fb43d6ebb
advgame <tsmithwd+advgit@gmail.com>
2015-12-21 19:39:00 -0500

    Fix no paren bug in main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var input = this.value;
         this.value = '';
         $('#output').append('<span class="oldInput">&amp;gt; ' + input +
-          '</span><span class="response">' + game.input(input) + '</span>';
+          '</span><span class="response">' + game.input(input) + '</span>');
         // TODO: game.input is a trusted function for this to work (no bad HTML)
       }
     });
