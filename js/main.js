314a68580d7b8d4e499336cbb412cb643c882726
advgame <tsmithwd+advgit@gmail.com>
2015-12-21 19:38:20 -0500

    Add tags to appended output in main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
       if (e.which === 13) {
         var input = this.value;
         this.value = '';
-        $('#output').val($('#output').val() + game.input(input));
+        $('#output').append('<span class="oldInput">&amp;gt; ' + input +
+          '</span><span class="response">' + game.input(input) + '</span>';
+        // TODO: game.input is a trusted function for this to work (no bad HTML)
       }
     });
   });
