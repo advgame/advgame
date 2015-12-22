f69e067ae4c8556ca3bd0dfecce3527ca4464153
advgame <tsmithwd+advgit@gmail.com>
2015-12-21 19:14:28 -0500

    Fix async bug in main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-  var game = window.loadGame('plot');
-  $('#input').keyup(function(e) {
-    if (e.which === 13) {
-      var input = this.value;
-      this.value = '';
-      $('#output').val($('#output').val() + game.input(input));
-    }
-  })
+  window.loadGame('plot', function(game) {
+    $('#input').keyup(function(e) {
+      if (e.which === 13) {
+        var input = this.value;
+        this.value = '';
+        $('#output').val($('#output').val() + game.input(input));
+      }
+    });
+  });
+  
 });
